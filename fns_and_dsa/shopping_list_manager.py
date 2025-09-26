770d8130eef6f107eca3fe0c37ed94b40df0ac5e
Sangiwe <nokubongasangiwe@gmail.com>
2025-09-26 05:20:15 -0700

    Fixed shopping list

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -9,22 +9,18 @@ def main():
     shopping_list = []
     while True:
         display_menu()
-        choice = input("Enter your choice: ")
+        choice = int(input("Enter your choice: "))
 
-        if choice == '1':
-            add_item = input("Enter item name: ")
+        if choice == 1:
+            add_item = input("Enter the item to add: ")
             shopping_list.append(add_item)
-            pass
-        elif choice == '2':
-            remove_item = input("Enter item name: ")
+        elif choice == 2:
+            remove_item = input("Enter the item to remove: ")
             shopping_list.remove(remove_item)
-            pass
-        elif choice == '3':
-            # Display the shopping list
+        elif choice == 3:
             for item in shopping_list:
                 print(item)
-            pass
-        elif choice == '4':
+        elif choice == 4:
             print("Goodbye!")
             break
         else:
