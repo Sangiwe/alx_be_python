f973bcd6a3d732c580afcebd2c9c01f00b002ec3
Sangiwe <nokubongasangiwe@gmail.com>
2025-10-01 21:28:49 +0200

    Fixed

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -13,7 +13,7 @@ class BankAccount:
             return False
             
     def display_balance(self):
-        print(f'Account Balance: {self.account_balance}')
+        print(f'Current Balance: {self.account_balance}')
             
 acc = BankAccount(200)
 acc.deposit(50)
