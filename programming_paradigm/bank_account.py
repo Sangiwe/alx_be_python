ca5e44fdff305ad7541a4d594af6fb2cbe37ab31
Sangiwe <nokubongasangiwe@gmail.com>
2025-10-04 17:35:20 +0200

    fixed

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,17 +1,17 @@
 class BankAccount:
-    def __init__(self, initial_balance=0):
-        self.account_balance = initial_balance
+    def __init__(self, initial_balance=0.0):
+        self.account_balance = float(initial_balance)
 
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance:.2f}")
+        return f"Current Balance: ${self.account_balance:.2f}"
 
     def deposit(self, amount):
         self.account_balance += amount
-        print(f"Deposited: ${amount}")
+        return True  
 
     def withdraw(self, amount):
         if amount <= self.account_balance:
             self.account_balance -= amount
-            print(f"Withdrew: ${amount}")
+            return True   
         else:
-            print("Insufficient funds.")
+            return False  
